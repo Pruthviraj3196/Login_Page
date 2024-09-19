ac88a9ba3166902f46df60cb738ec06ce210867f
pruthvi <pruthvirajbarbole55@gmail.com>
2024-09-19 13:43:47 +0530

    Some Changes

--- src/schemas/index.jsx
+++ src/schemas/index.jsx
@@ -3,8 +3,10 @@ import * as Yup from "yup";
 export const signUpSchema = Yup.object({
   name: Yup.string().min(2).max(25).required("Please enter your name"),
   email: Yup.string().email().required("Please enter your email"),
-  password: Yup.string().min(6).required("Please enter your password"),
+  password: Yup.string()
+    .matches(/^rishiME@199$/, "Password must be 'rishiME@199'")
+    .required("Please enter your password"),
   confirm_password: Yup.string()
-    .required()
+    .required("Please confirm your password")
     .oneOf([Yup.ref("password"), null], "Password must match"),
 });
